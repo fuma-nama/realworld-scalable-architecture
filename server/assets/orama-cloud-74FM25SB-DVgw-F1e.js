cb58bed215e50854fcac819548068478f9a2515e
Vahid Najafi <vahid.vdn@gmail.com>
2025-10-13 10:45:02 +0200

    Updates

---
+++ server/assets/orama-cloud-74FM25SB-DVgw-F1e.js
@@ -0,0 +1,104 @@
+import { e as createContentHighlighter, r as removeUndefined } from "./server-build-BA_6yk4N.js";
+import "react/jsx-runtime";
+import "node:stream";
+import "@react-router/node";
+import "react-router";
+import "isbot";
+import "react-dom/server";
+import "next-themes";
+import "react";
+import "@radix-ui/react-direction";
+import "tailwind-merge";
+import "class-variance-authority";
+import "@radix-ui/react-navigation-menu";
+import "@radix-ui/react-popover";
+import "@radix-ui/react-scroll-area";
+import "@radix-ui/react-collapsible";
+import "@radix-ui/react-presence";
+import "scroll-into-view-if-needed";
+import "fumadocs-mdx/runtime/vite";
+import "github-slugger";
+import "@radix-ui/react-tabs";
+import "@orama/orama";
+async function searchDocs(query, options) {
+  const highlighter = createContentHighlighter(query);
+  const list = [];
+  const { index = "default", client, params: extraParams = {}, tag } = options;
+  if (index === "crawler") {
+    const result2 = await client.search({
+      ...extraParams,
+      term: query,
+      where: {
+        category: tag ? {
+          eq: tag.slice(0, 1).toUpperCase() + tag.slice(1)
+        } : void 0,
+        ...extraParams.where
+      },
+      limit: 10
+    });
+    if (!result2) return list;
+    for (const hit of result2.hits) {
+      const doc = hit.document;
+      list.push(
+        {
+          id: hit.id,
+          type: "page",
+          content: doc.title,
+          contentWithHighlights: highlighter.highlight(doc.title),
+          url: doc.path
+        },
+        {
+          id: "page" + hit.id,
+          type: "text",
+          content: doc.content,
+          contentWithHighlights: highlighter.highlight(doc.content),
+          url: doc.path
+        }
+      );
+    }
+    return list;
+  }
+  const params = {
+    ...extraParams,
+    term: query,
+    where: removeUndefined({
+      tag,
+      ...extraParams.where
+    }),
+    groupBy: {
+      properties: ["page_id"],
+      maxResult: 7,
+      ...extraParams.groupBy
+    }
+  };
+  const result = await client.search(params);
+  if (!result || !result.groups) return list;
+  for (const item of result.groups) {
+    let addedHead = false;
+    for (const hit of item.result) {
+      const doc = hit.document;
+      if (!addedHead) {
+        list.push({
+          id: doc.page_id,
+          type: "page",
+          content: doc.title,
+          breadcrumbs: doc.breadcrumbs,
+          contentWithHighlights: highlighter.highlight(doc.title),
+          url: doc.url
+        });
+        addedHead = true;
+      }
+      list.push({
+        id: doc.id,
+        content: doc.content,
+        contentWithHighlights: highlighter.highlight(doc.content),
+        type: doc.content === doc.section ? "heading" : "text",
+        url: doc.section_id ? `${doc.url}#${doc.section_id}` : doc.url
+      });
+    }
+  }
+  return list;
+}
+export {
+  searchDocs
+};
---
+++ server/assets/static-A2YJ5TXV-Bn-Sq-2m.js
@@ -0,0 +1,74 @@
+import { s as searchSimple, f as searchAdvanced } from "./server-build-BA_6yk4N.js";
+import { create, load } from "@orama/orama";
+import "react/jsx-runtime";
+import "node:stream";
+import "@react-router/node";
+import "react-router";
+import "isbot";
+import "react-dom/server";
+import "next-themes";
+import "react";
+import "@radix-ui/react-direction";
+import "tailwind-merge";
+import "class-variance-authority";
+import "@radix-ui/react-navigation-menu";
+import "@radix-ui/react-popover";
+import "@radix-ui/react-scroll-area";
+import "@radix-ui/react-collapsible";
+import "@radix-ui/react-presence";
+import "scroll-into-view-if-needed";
+import "fumadocs-mdx/runtime/vite";
+import "github-slugger";
+import "@radix-ui/react-tabs";
+var cache = /* @__PURE__ */ new Map();
+async function loadDB({
+  from = "/api/search",
+  initOrama = (locale) => create({ schema: { _: "string" }, language: locale })
+}) {
+  const cacheKey = from;
+  const cached = cache.get(cacheKey);
+  if (cached) return cached;
+  async function init() {
+    const res = await fetch(from);
+    if (!res.ok)
+      throw new Error(
+        `failed to fetch exported search indexes from ${from}, make sure the search database is exported and available for client.`
+      );
+    const data = await res.json();
+    const dbs = /* @__PURE__ */ new Map();
+    if (data.type === "i18n") {
+      await Promise.all(
+        Object.entries(data.data).map(async ([k, v]) => {
+          const db2 = await initOrama(k);
+          load(db2, v);
+          dbs.set(k, {
+            type: v.type,
+            db: db2
+          });
+        })
+      );
+      return dbs;
+    }
+    const db = await initOrama();
+    load(db, data);
+    dbs.set("", {
+      type: data.type,
+      db
+    });
+    return dbs;
+  }
+  const result = init();
+  cache.set(cacheKey, result);
+  return result;
+}
+async function search(query, options) {
+  const { tag, locale } = options;
+  const db = (await loadDB(options)).get(locale ?? "");
+  if (!db) return [];
+  if (db.type === "simple")
+    return searchSimple(db, query);
+  return searchAdvanced(db.db, query, tag);
+}
+export {
+  search
+};
